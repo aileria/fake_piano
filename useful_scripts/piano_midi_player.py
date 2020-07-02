e3d9f022bbf27e9a2a9715305e557a7aa97c1022
aileria <aitorleria@gmail.com>
2020-07-02 19:31:48 +0200

    Inicializar repositorio

---
+++ useful_scripts/piano_midi_player.py
@@ -0,0 +1,12 @@
+import mido
+import time
+import fluidsynth
+
+file_path = 'midi_files/fantaisie.mid'
+port_name = 'Digital Piano:Digital Piano MIDI 1 24:0'
+
+# Reproducir MIDI
+outport = mido.open_output(port_name)
+mid = mido.MidiFile(file_path)
+for msg in mid.play():
+    outport.send(msg)
