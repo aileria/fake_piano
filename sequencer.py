1564e003190a876222f84b43e3fcb19f192d9aad
aileria <45357729+aileria@users.noreply.github.com>
2020-08-13 21:43:00 +0200

    Rehacer sequencer con asyncio

--- sequencer.py
+++ sequencer.py
@@ -1,7 +1,8 @@
+import asyncio
 import time
 from threading import Thread
 
-class Sequencer():
+class SyncSequencer():
 
     VELOCTIY = 60
 
@@ -23,3 +24,41 @@ class Sequencer():
 
     def stop(self):
         pass
+
+class Sequencer():
+
+    VELOCTIY = 60
+
+    def __init__(self, output):
+        self.output = output
+        self.thread = None
+        self.tasks = None
+
+    def add(self, notes, playback_speed):
+        self.stop()
+        self.thread = Thread(target=self.play, args=[notes, playback_speed])
+        self.thread.start()
+
+    def play(self, notes, playback_speed):
+        self.playback_speed = playback_speed
+        self.last_note_start = 0
+        try:
+            asyncio.run(self.process(notes))
+        except asyncio.exceptions.CancelledError:
+            pass
+
+    async def process(self, notes):
+        self.stop()
+        self.tasks = asyncio.gather(*[self.play_note(n) for n in notes])
+        await self.tasks
+
+    async def play_note(self, note):
+        await asyncio.sleep((note.start_time - self.last_note_start) / self.playback_speed)
+        self.last_note_start = note.start_time
+        self.output.note_on(note.value, Sequencer.VELOCTIY)
+        await asyncio.sleep(note.duration)
+        self.output.note_off(note.value)
+
+    def stop(self):
+        if self.tasks:
+            self.tasks.cancel()
